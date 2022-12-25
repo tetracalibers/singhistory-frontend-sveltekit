3113ef0ac5f5ecbd63d5a68a2ede2bc03880e246
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-12-25 16:57:50 +0900

    アクセストークンを付与したAPI呼び出しに対応

--- src/lib/api-client.ts
+++ src/lib/api-client.ts
@@ -5,21 +5,23 @@ type ApiMethod = 'GET' | 'POST' | 'PATCH' | 'DELETE'
 interface ApiResponse {
   statusCode: 409 | 500 | 201
   message: string
-  data?: unknown
+  data?: Record<string, string>
 }
 
 interface SendApiFnArgs {
   path: string
   method: ApiMethod
   body?: unknown
+  token?: string
 }
-const sendApi = async ({ path, method, body }: SendApiFnArgs) => {
+const sendApi = async ({ path, method, body, token }: SendApiFnArgs) => {
   const endpoint = `${API_BASE_URL}${path.startsWith('/') ? path : '/' + path}`
-  const options: RequestInit = {
-    mode: 'cors',
-    headers: { 'Content-Type': 'application/json' },
-    method
+  const headers: HeadersInit = {}
+  headers['Content-Type'] = 'application/json'
+  if (token) {
+    headers['Authorization'] = `Bearer ${token}`
   }
+  const options: RequestInit = { mode: 'cors', headers, method }
   if (body) {
     options.body = JSON.stringify(body)
   }
@@ -28,14 +30,14 @@ const sendApi = async ({ path, method, body }: SendApiFnArgs) => {
   return result
 }
 
-export const get = async (path: string) => {
-  return sendApi({ path, method: 'GET' })
+export const get = async (path: string, token?: string) => {
+  return sendApi({ path, method: 'GET', token })
 }
 
-export const post = async (path: string, body: unknown) => {
-  return sendApi({ path, method: 'POST', body })
+export const post = async (path: string, body: unknown, token?: string) => {
+  return sendApi({ path, method: 'POST', body, token })
 }
 
-export const patch = async (path: string, body: unknown) => {
-  return sendApi({ path, method: 'PATCH', body })
+export const patch = async (path: string, body: unknown, token?: string) => {
+  return sendApi({ path, method: 'PATCH', body, token })
 }
