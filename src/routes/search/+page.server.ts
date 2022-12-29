9efccbd4e73fe3e93e20d0b6fd0314a3b2023252
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-12-29 10:46:06 +0900

    load時に登録済みデータをfetchで取ってくる

--- src/routes/search/+page.server.ts
+++ src/routes/search/+page.server.ts
@@ -1,7 +1,26 @@
 import type { PageServerLoad } from './$types'
 import { getSpotifyAccessToken } from '@/lib/server/auth-spotify'
+import { SINGHIS_ACCESS_TOKEN_KEY } from '@/lib/constants'
+import * as api from '$lib/api-client'
 
-export const load: PageServerLoad = async () => {
-  const token = await getSpotifyAccessToken()
-  return { spotify: { token } }
+interface History {
+  id: string
+  artistName: string
+  songName: string
+  jacketUrl?: string
+  singDate: Date
+  singKey: number
+  rating: number
+  score?: number
+  memo?: string
+  createdAt: Date
+  updatedAt: Date
+}
+
+export const load: PageServerLoad = async ({ cookies }) => {
+  const spotify_token = await getSpotifyAccessToken()
+  const token = cookies.get(SINGHIS_ACCESS_TOKEN_KEY)
+  // TODO: 実際には、ページネーションの1ページ目のみ（最初に表示する分だけ）取得する
+  const history = await api.get<History>('sing-history', token)
+  return { spotify: { token: spotify_token }, data: { history: history.data } }
 }
