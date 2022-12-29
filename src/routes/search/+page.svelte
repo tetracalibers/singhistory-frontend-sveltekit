138b326fecdb091174e229717c43b2ff4efe451e
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-12-29 10:46:38 +0900

    検索仮ページの微修正とTODOメモ

--- src/routes/search/+page.svelte
+++ src/routes/search/+page.svelte
@@ -6,62 +6,79 @@
 
   const spotify = new SpotifySearch(data.spotify.token)
 
-  let artistQuery = ''
-  let songQuery = ''
-  let tracks: Track[] = []
-  let artists: Artist[] = []
-  let track: Track | null = null
-  let artist: Artist | null = null
+  let query: { track: string; artist: string } = { track: '', artist: '' }
+  let suggests: { track: Track[]; artist: Artist[] } = { track: [], artist: [] }
+  let selected: { track: Track | null; artist: Artist | null } = { track: null, artist: null }
 
-  const artistSearch = async () => {
-    const result = await spotify.artistSearch(artistQuery)
-    artists = result
-    const isExistTrackOwner = artists.some((_artist) => track?.artists?.[0]?.id === _artist.id)
+  // TODO: artistInputにfocusが当たったら登録済みartistを候補に出す？
+
+  // TODO: songの入力値はそのままで、songのsuggestは閉じる
+  // TODO: 登録したことのあるartistから取得する
+  // TODO: [API] 登録したことのあるartistをDBから取得する必要がある
+  const onArtistInput = async () => {
+    const result = await spotify.artistSearch(query.artist)
+    suggests.artist = result
+    // song選択中の場合、その曲のartistが今表示しているsuggest内に存在しないなら、song選択をリセットする
+    const isExistTrackOwner = suggests.artist.some(
+      (_artist) => selected.track?.artists?.[0]?.id === _artist.id
+    )
     if (!isExistTrackOwner) {
-      songQuery = ''
-      track = null
+      query.track = ''
+      selected.track = null
     }
   }
 
-  const songSearch = async () => {
-    const matchSongs = tracks.filter((track) => track.name.includes(songQuery))
+  // TODO: 選択済みartistの楽曲のみに絞り込む
+  // TODO: 選択済みartistの楽曲のうち、登録したことがあるsongからsuggest
+  // TODO: [API] artist選択の時点でそのartistの登録済みsongをDBから取得する必要がある
+  const onTrackInput = async () => {
+    // まずはsuggest中の曲から絞り込む
+    const matchSongs = suggests.track.filter((track) => track.name.includes(query.track))
     if (matchSongs.length > 0) {
-      tracks = matchSongs
+      suggests.track = matchSongs
       return
     }
-    const result = await spotify.songSearch(songQuery, artistQuery)
-    tracks = result
+    // suggest中の曲に合致するものがない場合、fetch
+    const result = await spotify.songSearch(query.track, query.artist)
+    suggests.track = result
   }
 
-  const selectArtist = async (_artist: Artist) => {
-    artist = _artist
-    artistQuery = _artist.name
-    artists = []
-    const hasSongs = await spotify.getArtistTracks(artist.name, songQuery)
-    tracks = hasSongs
+  // TODO: 選択済みのsongが選択済みartistの曲でないなら、song選択はリセットする
+  const onSelectArtist = async (_artist: Artist) => {
+    selected.artist = _artist
+    // 選択したartist名を自動入力
+    query.artist = _artist.name
+    suggests.artist = []
+    // song未選択の場合、選択したartistの曲でsong入力値に合致するものがないか調べる
+    const hasSongs = await spotify.getArtistTracks(selected.artist.name, query.track)
+    suggests.track = hasSongs
   }
 
-  const selectTrack = (_track: Track) => {
-    track = _track
-    songQuery = _track.name
-    tracks = []
+  // TODO: 登録したことのあるsongなら、その最新の履歴のsingkeyをデフォルト入力する
+  // TODO: [API] その曲の歌唱履歴をDBから取得する必要がある
+  const onSelectTrack = (_track: Track) => {
+    selected.track = _track
+    // 選択したsong名を自動入力
+    query.track = _track.name
+    suggests.track = []
     const thisArtist = _track.artists[0]
-    const matchArtist = artists.find((_artist) => _artist.id === thisArtist.id)
+    // 選択した曲のartistを選択状態にする
+    const matchArtist = suggests.artist.find((_artist) => _artist.id === thisArtist.id)
     if (matchArtist) {
-      artist = matchArtist
-      artistQuery = matchArtist.name
-      artists = []
+      selected.artist = matchArtist
+      query.artist = matchArtist.name
+      suggests.artist = []
     }
   }
 </script>
 
 <label for="artist-query">Artist</label>
-<div>selected: {artist?.name ?? ''}</div>
-<input id="artist-query" type="text" bind:value={artistQuery} on:change={artistSearch} />
+<div>selected: {selected.artist?.name ?? ''}</div>
+<input id="artist-query" type="text" bind:value={query.artist} on:change={onArtistInput} />
 <ul>
-  {#each artists as _artist}
+  {#each suggests.artist as _artist}
     <li>
-      <button type="button" on:click={() => selectArtist(_artist)}>
+      <button type="button" on:click={() => onSelectArtist(_artist)}>
         {_artist.name}
       </button>
     </li>
@@ -69,12 +86,12 @@
 </ul>
 
 <label for="song-query">Song</label>
-<div>selected: {track?.name ?? ''}</div>
-<input id="song-query" type="text" bind:value={songQuery} on:change={songSearch} />
+<div>selected: {selected.track?.name ?? ''}</div>
+<input id="song-query" type="text" bind:value={query.track} on:change={onTrackInput} />
 <ul>
-  {#each tracks as _track}
+  {#each suggests.track as _track}
     <li>
-      <button type="button" on:click={() => selectTrack(_track)}>
+      <button type="button" on:click={() => onSelectTrack(_track)}>
         {_track.name}
       </button>
     </li>
