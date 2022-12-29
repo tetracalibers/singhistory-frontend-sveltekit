5188b9d6a01b95f1364177b90a4a9c8cb88df99b
Kanno Ami (tomixy) <tomix05may@gmail.com>
2022-12-29 10:45:35 +0900

    トップページをコンポーネント表示確認用に整備

--- src/routes/+page.server.ts
+++
@@ -1,9 +0,0 @@
-import type { PageServerLoad } from './$types'
-import * as api from '$lib/api-client'
-import { SINGHIS_ACCESS_TOKEN_KEY } from '@/lib/constants'
-
-export const load: PageServerLoad = async ({ cookies }) => {
-  const token = cookies.get(SINGHIS_ACCESS_TOKEN_KEY)
-  const history = await api.get('sing-history', token)
-  console.log(history)
-}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,34 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import StarRatingInput from '@/features/rating/star-rating-input.svelte'
+</script>
+
+<div class="app-root">
+  <div class="modal-root">
+    <StarRatingInput />
+  </div>
+</div>
+
+<style>
+  .app-root {
+    width: 100vw;
+    min-height: 100vh;
+    position: relative;
+    background-image: linear-gradient(
+        221deg,
+        rgba(12, 186, 186, 0.9) 0%,
+        rgba(100, 125, 238, 0.9) 74%
+      ),
+      url(image/texture/twinkle-back.jpg);
+    background-size: cover;
+  }
+
+  .modal-root {
+    background-color: rgba(255, 255, 255, 0);
+    background-image: linear-gradient(
+      315deg,
+      rgba(249, 197, 209, 0.49) 0%,
+      rgba(151, 149, 239, 0.48) 74%
+    );
+    backdrop-filter: blur(20px) drop-shadow(white 2px 4px 6px);
+  }
+</style>
